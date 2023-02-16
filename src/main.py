b67cb820ed0b9790f34a8abecd2feaf57e89aa36
andrewfarmer13 <andrewfarmer13@gmail.com>
2023-02-16 15:16:55 -0600

    updated main.py

--- src/main.py
+++ src/main.py
@@ -4,5 +4,7 @@ if __name__ == "__main__":
     file = input("What file would you like to read: ")
     if(file == "SquareGame.jack"):
         tk.readFile(file)
+    elif(file == "Main.jack"):
+        tk.readFile(file)
     else:
         print("File does not exist, shutting down now!")
