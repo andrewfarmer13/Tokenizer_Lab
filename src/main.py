c28656f2968e432c755b7ad25c7313e73c944882
andrewfarmer13 <andrewfarmer13@gmail.com>
2023-02-18 23:20:56 -0600

    Ignore Comments Sorta

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import tokenizer as tk
 if __name__ == "__main__":
     file = input("What file would you like to read: ")
     if(file == "SquareGame.jack"):
-        tk.readFile(file)
+       f = tk.readFile(file)
+       print(f)
     elif(file == "Main.jack"):
-        tk.readFile(file)
+       f = tk.readFile(file)
+       print(f)
+
     else:
         print("File does not exist, shutting down now!")
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,4 +1,37 @@
+outputBuffer = []
+counter = 0
 def readFile(fileIn):
     f = open(fileIn, "r")
-    print(f.read())
+    # for char in f:
+    #     c = getChar(f)
+    #     counter += 1
+    #     if(c == "/"):
+    #         while True:
+    #             c2 = nextChar(f)
+    #             if(c2 != " " and (nextChar(f)) != None):
+    #                pass
+    #     else:
+    #         outputBuffer.append(c)
+
+    # lines = f.readlines()
+    # for lines in enumerate(lines):
+    #     line = lines.readline()
+    #     if lines[0] != "/":
+    #         outputBuffer.append(line)
+        
+
     
+    for lines in f:
+        line = f.readline()
+        if line[0] != "/":
+            outputBuffer.append(line)
+
+    return(outputBuffer)
+
+def getChar(f):
+    char = f[counter]
+    return(char)
+
+def nextChar(f):
+    char = f[counter + 1]
+    return(char)
